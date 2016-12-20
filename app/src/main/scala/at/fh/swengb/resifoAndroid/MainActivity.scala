60cc1546aeefec01c17e43a1f8188e73aa31d329
laszlobalo <laszlo.balo@edu.fh-joanneum.at>
2016-12-20 14:53:31 +0100

    Initial commit

---
+++ app/src/main/scala/at/fh/swengb/resifoAndroid/MainActivity.scala
@@ -0,0 +1,15 @@
+/**
+  * Created by laszlobalo on 20.12.16.
+  */
+package at.fh.swengb.resifoandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import at.fh.swengb.resifoandroid.R
+
+class MainActivity extends AppCompatActivity {
+  override protected def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
